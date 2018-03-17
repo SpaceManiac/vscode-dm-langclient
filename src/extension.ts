28d8f78600302473154800b71fa3f39d89886447
Tad Hardesty <tad@platymuus.com>
2018-03-17 01:52:20 -0700

    Add some framework for the updater

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as fs from 'fs';
 import { workspace, window, ExtensionContext } from 'vscode';
 import * as languageclient from 'vscode-languageclient';
 
-import { promisify } from './misc';
+import { promisify, is_executable, md5_file } from './misc';
 
 export async function activate(context: ExtensionContext) {
 	await start_language_client(context);
@@ -37,34 +37,62 @@ async function start_language_client(context: ExtensionContext) {
 
 async function determine_server_command(context: ExtensionContext): Promise<string | undefined> {
 	// If the config override is set, use that, and don't autoupdate
-	let serverCommand: string | undefined = workspace.getConfiguration('dreammaker').get('langserverPath');
+	const serverCommand: string | undefined = workspace.getConfiguration('dreammaker').get('langserverPath');
 	if (serverCommand) {
-		try {
-			console.log(await promisify(fs.access)(serverCommand, fs.constants.R_OK | fs.constants.X_OK));
+		if (await is_executable(serverCommand)) {
 			return serverCommand;
-		} catch (e) {
-			// Error indicates that the serverCommand isn't valid, fall through.
+		} else {
+			return await prompt_for_server_command(context, "Configured executable is missing or invalid.");
 		}
-		return await prompt_for_server_command(context, "Configured executable is missing or invalid.");
 	}
 
-	// Nothing is set in the config. What are you gonna do?
-	return await prompt_for_server_command(context, "No executable configured.");
+	// Nothing is set in the config.
+	const arch = os.arch(), platform = os.platform();
+	const extension = (platform == 'win32') ? ".exe" : "";
+	const auto_file = `${context.extensionPath}/bin/dm-langserver-${arch}-${platform}${extension}`;
+	const update_file = `${auto_file}.update`;
+	if (await is_executable(`${auto_file}.update`)) {
+		await promisify(fs.rename)(update_file, auto_file);
+	}
+
+	if (await is_executable(auto_file)) {
+		// If the executable is already valid, run it now, and update later.
+		auto_update(context, platform, arch, update_file, await md5_file(auto_file));
+		return auto_file;
+	} else {
+		// Otherwise, update now.
+		await auto_update(context, platform, arch, auto_file, null);
+	}
 }
 
 async function prompt_for_server_command(context: ExtensionContext, message: string): Promise<string | undefined> {
 	// Show an info/confirmation before browsing...
-	let choice = await window.showInformationMessage(`The dm-langserver executable must be selected. ${message}`, "Browse", "Cancel");
+	const choice = await window.showInformationMessage(`The dm-langserver executable must be selected. ${message}`, "Browse", "Cancel");
 	if (choice === "Cancel") {
 		return undefined;
 	}
 	// Browse for the executable...
-	let list = await window.showOpenDialog({});
+	const list = await window.showOpenDialog({});
 	if (!list) {
 		return undefined;
 	}
 	/// And update the config.
-	let path = list[0].fsPath;
+	const path = list[0].fsPath;
 	workspace.getConfiguration('dreammaker').update('langserverPath', path, true);
 	return path;
+}
+
+async function auto_update(context: ExtensionContext, platform: string, arch: string, out_file: string, hash: string | null): Promise<string | undefined> {
+	let url = `https://wombat.platymuus.com/ss13/dm-langserver/update.php?platform=${platform}&arch=${arch}`;
+	if (hash) {
+		url += `&hash=${hash}`;
+	}
+	console.log(url);
+
+	// If hash provided, notify on success. Otherwise, notify on failure.
+	if (!hash) {
+		return await prompt_for_server_command(context, `Binaries are not available for ${arch}-${platform}.`);
+	} else {
+		return undefined;
+	}
 }
--- src/misc.ts
+++ src/misc.ts
@@ -1,6 +1,9 @@
 'use strict';
 
-export function promisify(original_function: Function): Function {
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+
+export function promisify(func: Function, this_: any = undefined): Function {
 	return (...args: any[]) => {
 		return new Promise((resolve, reject: any) => {
 			args.push((err: any, ...rest: any[]) => {
@@ -14,7 +17,7 @@ export function promisify(original_function: Function): Function {
 				}
 			});
 			try {
-				original_function.apply(undefined, args);
+				func.apply(this_, args);
 			} catch (err) {
 				if (reject) {
 					reject(err);
@@ -23,4 +26,26 @@ export function promisify(original_function: Function): Function {
 			}
 		});
 	};
+}
+
+export async function is_executable(path: string): Promise<boolean> {
+	try {
+		await promisify(fs.access)(path, fs.constants.R_OK | fs.constants.X_OK);
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+export async function md5_file(path: string): Promise<string> {
+	let output = crypto.createHash('md5');
+	let input = fs.createReadStream(path);
+	input.pipe(output);
+	await promisify(output.once, output)('readable');
+	let read = output.read();
+	if (typeof read === 'string') {
+		return read;
+	} else {
+		return read.toString('hex');
+	}
 }
